3c772cf06da7439acaed4ec54e8e6d7cb8502e78
nakamuratakeru <nakamuratakeru@localhost>
2023-05-12 14:59:19 +0900

    aa

---
+++ src/main/java/entity/Employee.java
@@ -0,0 +1,38 @@
+package entity;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "employee")
+public class Employee {
+
+    /** 主キー。自動生成 */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    /** 名前。20桁。null不許可 */
+    @Column(length = 20, nullable = false)
+    private String name;
+
+    private Integer delete_flag;
+
+    private LocalDateTime created_at;
+
+    private LocalDateTime updated_at;
+
+    //@OneToOne(mappedBy="employee")
+    //private Authentication authentication;
+
+}
